e8f639f8e1edd6f9eb21a72adcd1cec953a65b70
FiroRos <ofir.workmode@gmail.com>
2024-12-19 21:07:31 +0200

    compile

--- config/boards/shields/paintbrush/test_widget.h
+++ config/boards/shields/paintbrush/test_widget.h
@@ -1,7 +1,7 @@
 #ifndef ZMK_WIDGET_TEST_WIDGET_H
 #define ZMK_WIDGET_TEST_WIDGET_H
 
-#include <lvgl.h>
+#include "lvgl.h"
 
 // Declare the widget functions
 void test_widget_init(lv_obj_t *parent);
